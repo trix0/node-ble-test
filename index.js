089f9bcc46872564a09300b9e0e220ab3acb9028
Matúš Šatara <mas@o2matic.com>
2022-08-06 15:53:38 +0200

    setup

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const {createBluetooth} = require('node-ble')
+const {bluetooth, destroy} = createBluetooth()
+
+
+
+
+
+
+
+
+async function main(){
+const adapter = await bluetooth.defaultAdapter()
+if (! await adapter.isDiscovering())
+  await adapter.startDiscovery()
+
+
+
+
+
+setInterval(async()=>{
+let devices=await adapter.devices()
+console.log(devices)
+adapter.devices(devices=>{
+console.log(devices)
+})
+},2000)
+}
+main()
